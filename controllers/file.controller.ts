8ef17d0753e5fa658e3db703655d4d62ebfcd445
ArthurVerrept <arthur-verrept@hotmail.co.uk>
2022-06-08 16:03:55 +0100

    slightly better ui

--- controllers/file.controller.ts
+++ controllers/file.controller.ts
@@ -6,20 +6,33 @@ import path from 'path'
 const baseUrl = 'http://localhost:3000/files/'
 const __baseUrl = path.join(__dirname, '../')
 
+async function deleteFile(req: express.Request, res: express.Response) {
+  const directoryPath = __baseUrl + 'resources/static/assets/uploads/' + req.body.nameçç
+  fs.unlink(directoryPath, (err => {
+    if (err) console.log(err);
+    else {
+      res.status(200).send({
+        message: 'file deleted successfully'
+      })
+    }
+  }));
+
+}
+
 async function upload(req: express.Request, res: express.Response) {
   try {
     await uploadFile(req, res)
-    if (req.file == undefined) {
+    if (req.files == undefined) {
       return res.status(400).send({ message: 'Please upload a file!' })
     }
     console.log('PAST UPLOAD FILE')
     res.status(200).send({
-      message: 'Uploaded the file successfully: ' + req.file.originalname,
+      message: 'Uploaded the files successfully'
     })
   } catch (err) {
-    if(req.file) {
+    if (req.files) {
       res.status(500).send({
-        message: `Could not upload the file: ${req.file.originalname}. ${err}`,
+        message: `Could not upload the files: ${err}`,
       })
     }
   }
@@ -54,8 +67,10 @@ const download = (req: express.Request, res: express.Response) => {
     }
   })
 }
+
 export default {
   upload,
   getListFiles,
   download,
+  deleteFile
 }
